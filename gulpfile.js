ee1304e2dbf19f462a8c22cf10f12b2526167751
svensktutby <svensk.tut.by@gmail.com>
2019-04-26 23:15:03 +0300

    SCSS compile was added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,72 @@
 "use strict";
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+const gulp = require('gulp'),
+      plumber = require('gulp-plumber'),
+      sourcemaps = require('gulp-sourcemaps'),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      rename = require('gulp-rename'),
+      posthtml = require('gulp-posthtml'),
+      include = require('posthtml-include'),
+      del = require('del'),
+      server = require('browser-sync').create();
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+/* Plumber error message
+ ******************************/
+const plumberLog = function(error) {
+  console.log([
+    '',
+    '----------ERROR MESSAGE START----------',
+    ('[' + error.name + ' in ' + error.plugin + ']'),
+    error.message,
+    '----------ERROR MESSAGE END----------',
+    ''
+  ].join('\n'));
+  this.end();
+};
+
+/* Styles compile
+******************************/
+gulp.task('styles', function () {
+  return gulp.src('./source/scss/main.scss')
     .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(sourcemaps.init())
+    .pipe(sass()).on('error', plumberLog)
+    .pipe(autoprefixer({
+      browsers: [
+        'last 3 versions',
+        'Android 2.3',
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24',
+        'Explorer >= 10',
+        'iOS >= 6',
+        'Opera >= 11',
+        'Safari >= 4'
+      ],
+      cascade: false
+    }))
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./source/css'))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+/* Server
+ ******************************/
+gulp.task('serve', function () {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: {
+      port: 3000,
+      baseDir: './source'
+    },
+    notify: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch('./source/scss/**/*.scss', gulp.series('styles'));
+  gulp.watch('./source/*.html').on('change', server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+/* Default
+******************************/
+gulp.task('default', gulp.series('styles', 'serve'));
